c547e94e85d16c20dc519d94204d52355a880873
Afito Indra Permana <indraafito56@gmail.com>
2026-05-29 19:55:28 +0700

    Update README and add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="id">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+      </Head>
       <body className="antialiased text-primary-dark font-sans">
         <Main />
         <NextScript />
